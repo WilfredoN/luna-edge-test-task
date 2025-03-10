cbbd31e3771b42e6f6a1740db220c6392fe88dc3
WilfredoN <thekillermaps@gmail.com>
2025-03-10 12:35:08 +0200

    feat: base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 
 function App() {
   return (
-    <div>
-      <p className="text-5xl">Welcome to Luna Edge technical interview</p>
-    </div>
+    <main className="bg-gray-800 w-screen h-screen">
+      <div className="flex items-center justify-center h-full">
+      <p className="text-5xl text-gray-50">Welcome to Luna Edge technical interview</p>
+      </div>
+        
+    </main>
   )
 }
 
